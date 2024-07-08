78cdf22a368902867dbc90737a672aa4d3d7d38e
Iliya Brook <iliyabrook1987@gmail.com>
2024-07-08 18:05:55 +0300

    before redeploy 1

--- web/frontend/vite.config.js
+++ web/frontend/vite.config.js
@@ -1,63 +1,77 @@
-import { defineConfig } from "vite";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import https from "https";
-import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from 'vite';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const FRONTEND_PORT = process.env.FRONTEND_PORT;
 
 if (
-  process.env.npm_lifecycle_event === "build" &&
-  !process.env.CI &&
-  !process.env.SHOPIFY_API_KEY
+	process.env.npm_lifecycle_event === 'build' &&
+	!process.env.CI &&
+	!process.env.SHOPIFY_API_KEY
 ) {
-  console.warn(
-    "\nBuilding the frontend app without an API key. The frontend build will not run without an API key. Set the SHOPIFY_API_KEY environment variable when running the build command.\n"
-  );
+	console.warn(
+		'\nBuilding the frontend app without an API key. The frontend build will not run without an API key. Set the SHOPIFY_API_KEY environment variable when running the build command.\n',
+	);
 }
 
 const proxyOptions = {
-  target: `http://127.0.0.1:${process.env.BACKEND_PORT}`,
-  changeOrigin: false,
-  secure: true,
-  ws: false,
+	target: `http://127.0.0.1:${process.env.BACKEND_PORT}`,
+	changeOrigin: false,
+	secure: true,
+	ws: false,
 };
 
 const host = process.env.HOST
-  ? process.env.HOST.replace(/https?:\/\//, "")
-  : "localhost";
+	? process.env.HOST.replace(/https?:\/\//, '')
+	: 'localhost';
 
 let hmrConfig;
-if (host === "localhost") {
-  hmrConfig = {
-    protocol: "ws",
-    host: "localhost",
-    port: 64999,
-    clientPort: 64999,
-  };
+if (host === 'localhost') {
+	hmrConfig = {
+		protocol: 'ws',
+		host: 'localhost',
+		port: 64999,
+		clientPort: 64999,
+	};
 } else {
-  hmrConfig = {
-    protocol: "wss",
-    host: host,
-    port: process.env.FRONTEND_PORT,
-    clientPort: 443,
-  };
+	hmrConfig = {
+		protocol: 'wss',
+		host: host,
+		port: FRONTEND_PORT,
+		clientPort: 443,
+	};
 }
 
-export default defineConfig({
-  root: dirname(fileURLToPath(import.meta.url)),
-  plugins: [react()],
-  define: {
-    "process.env.SHOPIFY_API_KEY": JSON.stringify(process.env.SHOPIFY_API_KEY),
-  },
-  resolve: {
-    preserveSymlinks: true,
-  },
-  server: {
-    host: "localhost",
-    port: process.env.FRONTEND_PORT,
-    hmr: hmrConfig,
-    proxy: {
-      "^/(\\?.*)?$": proxyOptions,
-      "^/api(/|(\\?.*)?$)": proxyOptions,
-    },
-  },
+
+// noinspection JSCheckFunctionSignatures
+export default defineConfig(({ mode }) => {
+	// VITE_APP client environments
+	const {
+		VITE_APP_HOST:HOST,
+		VITE_APP_PORT:PORT,
+		VITE_APP_PUBLIC_URL:URL
+	} = loadEnv(mode, process.cwd());
+	
+	return {
+		root: dirname(fileURLToPath(import.meta.url)),
+		plugins: [react()],
+		define: {
+			"process.env.SHOPIFY_API_KEY": JSON.stringify(process.env.SHOPIFY_API_KEY),
+		},
+		resolve: {
+			preserveSymlinks: true,
+		},
+		server: {
+			host: HOST || "localhost",
+			port: PORT || process.env.FRONTEND_PORT,
+			hmr: hmrConfig,
+			proxy: {
+				"^/(\\?.*)?$": proxyOptions,
+				"^/api(/|(\\?.*)?$)": proxyOptions,
+			},
+		}
+	}
 });
--- web/index.js
+++ web/index.js
@@ -2,7 +2,7 @@ import { join } from 'path';
 import { readFileSync } from 'fs';
 import express from 'express';
 import serveStatic from 'serve-static';
-
+import cors from 'cors';
 import shopify from './shopify.js';
 import webhooks from './webhooks.js';
 
@@ -15,23 +15,55 @@ const STATIC_PATH =
 
 const app = express();
 
-// Set up Shopify authentication and webhook handling
+app.use(cors({
+	origin: '*'
+}));
+
+app.use(express.json());
+
+// add shop query parameter to all requests function if shop query param not exist
+const addDefaultShopParam = (req, res, next) => {
+	const isApiRequest = req.path.startsWith('/api');
+	if (!req.query.shop && !isApiRequest) {
+		req.query.shop = process.env.SHOP;
+	}
+	next();
+};
+
+
 app.get(shopify.config.auth.path, shopify.auth.begin());
 app.get(
 	shopify.config.auth.callbackPath,
 	shopify.auth.callback(),
 	shopify.redirectToShopifyOrAppRoot()
 );
+
+// route to import cart
+app.post('/api/import_cart', (req, res) => {
+	const customerId = req.body.customer_id;
+	console.log("customerId:", customerId)
+	console.log("req:", req)
+	
+	const mockedData = {
+		items: [
+			{ id: 1, title: 'Product 1', quantity: 2 },
+			{ id: 2, title: 'Product 2', quantity: 1 }
+		]
+	}
+	
+	res.json(mockedData);
+});
+
 app.post(
 	shopify.config.webhooks.path,
-	// @ts-ignore
 	shopify.processWebhooks({ webhookHandlers: webhooks })
 );
 
-// All endpoints after this point will require an active session
-app.use('/api/*', shopify.validateAuthenticatedSession());
+// add shop query parameter to all requests
+app.use(addDefaultShopParam);
 
-app.use(express.json());
+// validate the session for all /api requests
+app.use('/api/*', shopify.validateAuthenticatedSession());
 
 app.use(serveStatic(STATIC_PATH, { index: false }));
 
@@ -39,4 +71,6 @@ app.use('/*', shopify.ensureInstalledOnShop(), async (_req, res) => {
 	return res.set('Content-Type', 'text/html').send(readFileSync(join(STATIC_PATH, 'index.html')));
 });
 
-app.listen(PORT);
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
